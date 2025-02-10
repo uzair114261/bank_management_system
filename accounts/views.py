baa40311ecab24877580595bfc4fcbf10bc4646f
Muhammad Uzair <rajputuzair58@gmail.com>
2025-02-10 11:52:45 +0500

    Added form for adding banks and user accounts Implemented bank and account listing in the system

---
+++ accounts/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import redirect
+from .forms import  AccountForm
+from .models import Accounts
+from django.views.generic import  ListView
+
+# Create your views here.
+class AccountView(ListView):
+    model = Accounts
+    template_name = 'accounts/index.html'
+    context_object_name = 'accounts'
+
+    def get(self, request, *args, **kwargs):
+        super().get(request, *args, **kwargs)
+        form = AccountForm()
+        context = self.get_context_data()
+        context['form'] = form
+        return self.render_to_response(context)
+
+    def post(self, request, *args, **kwargs):
+        form = AccountForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('account_list')
+        context = self.get_context_data()
+        context['form'] = form
+        return self.render_to_response(context)
