ce61d07d8c6e713b29c30c99ee09b8caabc120e4
Muhammad Uzair <rajputuzair58@gmail.com>
2025-02-11 11:41:22 +0500

    Resolved: renamed the related_name from account_holder to account. and revert the migrations to aim for only one migration in PR.

--- accounts/models.py
+++ accounts/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Account(models.Model):
     bank = models.ForeignKey(Bank, on_delete=models.CASCADE, related_name='bank')
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='account_holder')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='account')
     balance = models.PositiveIntegerField()
 
     def __str__(self):
