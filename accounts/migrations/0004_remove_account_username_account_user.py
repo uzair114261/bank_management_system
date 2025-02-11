dce78493f8f85cdfbf33ebd690bc2b830f3be7fb
Muhammad Uzair <rajputuzair58@gmail.com>
2025-02-11 12:15:11 +0500

    Resolved: changed the default=1 to null=True.

--- accounts/migrations/0004_remove_account_username_account_user.py
+++ accounts/migrations/0004_remove_account_username_account_user.py
@@ -20,7 +20,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='account',
             name='user',
-            field=models.ForeignKey(default=1, on_delete=django.db.models.deletion.CASCADE, related_name='account', to=settings.AUTH_USER_MODEL),
+            field=models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, related_name='account', to=settings.AUTH_USER_MODEL),
             preserve_default=False,
         ),
     ]
